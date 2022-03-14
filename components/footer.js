6f87b0e31572be87ce1dd68049f9ac6a223e9b1e
novrandobilly <novrandobilly@gmail.com>
2022-03-14 11:22:59 +0700

    nvn-Create footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,68 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../assets/logo-color.png';
+import Instagram from '../assets/icons/instagram-blue.svg';
+import Facebook from '../assets/icons/facebook-blue.svg';
+import Linkedin from '../assets/icons/linkedin-blue.svg';
+import styles from './footer.module.scss';
 
 export default function footer() {
-  return <div>footer</div>;
+  return (
+    <div className={styles['container']}>
+      <div className={styles['menu']}>
+        <div className={styles['submenu']}>
+          <div className={styles['link-list']}>
+            <h3>Product</h3>
+            <Link href='/product'>Product</Link>
+            <Link href='/product'>Product</Link>
+            <Link href='/product'>Product</Link>
+          </div>
+          <div className={styles['link-list']}>
+            <h3>Company</h3>
+            <Link href='/company'>Company</Link>
+            <Link href='/company'>Company</Link>
+            <Link href='/company'>Company</Link>
+          </div>
+          <div className={styles['link-list']}>
+            <h3>Career</h3>
+            <Link href='/career'>Career</Link>
+            <Link href='/career'>Career</Link>
+            <Link href='/career'>Career</Link>
+          </div>
+          <div className={styles['link-list']}>
+            <h3>Highlights</h3>
+            <Link href='/highlights'>Highlights</Link>
+            <Link href='/highlights'>Highlights</Link>
+            <Link href='/highlights'>Highlights</Link>
+          </div>
+          <div className={styles['link-list']}>
+            <h3>Support</h3>
+            <Link href='/support'>Support</Link>
+            <Link href='/support'>Support</Link>
+            <Link href='/support'>Support</Link>
+          </div>
+        </div>
+        <div className={styles['copyrights']}>
+          <p>Matrix NAP Info, copyrights 2021</p>
+        </div>
+      </div>
+      <div className={styles['company-information']}>
+        <div className={styles['footer-logo']}>
+          <Image alt='Logo' src={Logo} width={150} height={65} />
+        </div>
+        <p className={styles['company-description']}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, voluptate optio.
+        </p>
+        <h4>
+          <strong>Follow our socials!</strong>
+        </h4>
+        <div className={styles['social-media']}>
+          <Image alt='Instagram' src={Instagram} width={30} height={30} />
+          <Image alt='Faccebook' src={Facebook} width={30} height={30} />
+          <Image alt='Linkedin' src={Linkedin} width={30} height={30} />
+        </div>
+      </div>
+    </div>
+  );
 }
