8f73d40094aad4a71835f0c8ab75b42dfba03fb4
novrandobilly <novrandobilly@gmail.com>
2022-03-31 16:56:25 +0700

    nvn-Refine Send Message logic using useState

--- components/business-solution/contact-us.js
+++ components/business-solution/contact-us.js
@@ -1,12 +1,29 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 import styles from './contact-us.module.scss';
 
 export default function ContactUs() {
-  const nameRef = useRef(null);
-  const phoneRef = useRef(null);
-  const emailRef = useRef(null);
-  const messageRef = useRef(null);
+  const [nameValue, setNameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [messageValue, setMessageValue] = useState('');
+  const [phoneValue, setPhoneValue] = useState('');
+
+  const nameChangeHandler = (e) => {
+    const value = e.target.value;
+    setNameValue(value);
+  };
+  const emailChangeHandler = (e) => {
+    const value = e.target.value;
+    setEmailValue(value);
+  };
+  const phoneChangeHandler = (e) => {
+    const value = e.target.value;
+    setPhoneValue(value);
+  };
+  const messageChangeHandler = (e) => {
+    const value = e.target.value;
+    setMessageValue(value);
+  };
 
   // const submitHandler = (event) => {
   //   event.preventDefault();
@@ -27,21 +44,17 @@ export default function ContactUs() {
         <p>Complete the form and our representative will contact you shortly</p>
         <div className={styles['input-group']}>
           <div className={styles['short-input']}>
-            <input type='text' placeholder='Your Name' ref={nameRef} />
-            <input type='text' placeholder='Phone Number' ref={phoneRef} />
-            <input type='text' placeholder='Email' ref={emailRef} />
+            <input type='text' placeholder='Your Name' onChange={nameChangeHandler} value={nameValue} />
+            <input type='text' placeholder='Phone Number' onChange={phoneChangeHandler} value={phoneValue} />
+            <input type='text' placeholder='Email' onChange={emailChangeHandler} value={emailValue} />
           </div>
-          <textarea placeholder='Message' cols='30' ref={messageRef}></textarea>
+          <textarea placeholder='Message' cols='30' onChange={messageChangeHandler} value={messageValue}></textarea>
         </div>
         <a
           target='_blank'
           rel='noreferrer'
           className={styles['send-message']}
-          href={`mailto:info@napinfo.co.id?cc=customer.care@napinfo.co.id&?subject=Inquiry%20From%20Website&body=Name%3A%20${
-            nameRef.current?.value || ''
-          }%0D%0APhone%20Number%3A%20${phoneRef.current?.value || ''}%0D%0AEmail%3A%20${
-            nameRef.current?.value || ''
-          }%0D%0AMessage%3A%0D%0A${messageRef.current?.value || ''}`}>
+          href={`mailto:info@napinfo.co.id?cc=customer.care@napinfo.co.id&?subject=Inquiry%20From%20Website&body=Name%3A%20${nameValue}%0D%0APhone%20Number%3A%20${phoneValue}%0D%0AEmail%3A%20${emailValue}%0D%0AMessage%3A%0D%0A${messageValue}`}>
           Send Message
         </a>
       </form>
